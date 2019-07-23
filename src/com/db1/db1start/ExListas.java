8c35ed08064fac799fa22859e0045782f5d0bd0d
Guilherme <guiii@live.com>
2019-07-23 10:57:40 -0300

    Update exercícios de listas - aula 11

--- src/com/db1/db1start/ExListas.java
+++ src/com/db1/db1start/ExListas.java
@@ -1,6 +1,7 @@
 package com.db1.db1start;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class ExListas {
 	
@@ -15,27 +16,88 @@ public class ExListas {
 	}
 	
 	// Método que passado uma lista retorne a quantidade de itens.
+	public int contaArray(List<String> lista) {
+		int tamanhoLista = lista.size();
+		return tamanhoLista;
+	}
 
 	
 	// Método que receba 3 Strings adicione a uma lista e remova o valor da segunda posição.
-	
+	public List<String> addListArray(String item1, String item2, String item3) {
+		List<String> itens = new ArrayList<String>();
+		itens.add(item1);
+		itens.add(item2);
+		itens.add(item3);
+		itens.remove(1);
+		return itens;
+	}
 	
 	
 	// Método que apresente os nomes das cores do exercicio 1 ordenados.
-	
+	public List<String> ordenaArrayList() {
+		List<String> cores = minhasCores();
+		Collections.sort(cores);
+		return cores;
+	}
 	
 	
 	// Método que receba uma lista com as cores que você mais gosta e 
 	// um atributo que receba uma cor a ser removida.
 	
+	public List<String> recebeListaDeletaItem(List<String> lista, String deleta) {
+		lista.remove(deleta);
+		return lista;
+	}
+	
 	
 	
 	// Método que recebido uma lista de String, retorne ordenado de forma decrescente
-	
+	public List<String> inverteArrayList() {
+		List<String> cores = minhasCores();
+		Collections.sort(cores);	// primeiro ordena
+		Collections.reverse(cores);	// depois inverte a ordem
+		return cores;
+	}
 	
 	
 	// Método que receba uma lista de números inteiros e retorne uma lista de lista, 
 	// contendo em uma das listas os números pares e em outra lista o números ímpares
+	
+	// Método que receba a seguinte quantidade de dados e exiba os nomes distintos pelo primeiro caracter e ordenados
+	
+	
+	
+	// Método que receba uma lista de Integer e retorna a soma.
+			// criação da lista de números:
+	public List<Integer> meusNumeros() {
+		List<Integer> listaNum = new ArrayList<Integer>();
+		listaNum.add(2);
+		listaNum.add(5);
+		listaNum.add(4);
+		listaNum.add(3);
+		listaNum.add(1);
+		return listaNum;
+	}
+	
+			// criação do método
+	public int somaLista(List<Integer> numeros) {
+		int soma = 0;
+		for(int i = 0; i < numeros.size(); i++)
+		    soma = soma + numeros.get(i);
+		return soma;
+	}
+	
+	
+	
+	// Método que receba uma lista de double e retorne a média.
+	
+	// Método que receba uma lista de Integer e devolva o menor valor.
+	
+	// Método que receba uma lista de Integer e devolva o maior valor.
+	
+	// Método que receba uma lista de Integer e remova todos os ímpares.
+	
+	// Método que receba uma frase e retorne todas as vogais.
 	
 	
 	
--- test/com/db1/db1start/ExListasTest.java
+++ test/com/db1/db1start/ExListasTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ExListasTest {
 	
 	@Test
-	public void deveMinhasCoresFavoritas() {
+	public void deveRetornarMinhasCoresFavoritas() {
 		ExListas exListas = new ExListas();
 		
 		List<String> cores = exListas.minhasCores();
@@ -17,5 +17,59 @@ public class ExListasTest {
 		Assert.assertTrue(cores.contains("Preto"));
 		Assert.assertTrue(cores.contains("Azul"));
 	}
+	
+	@Test 
+	public void deveContarItensLista() {
+		ExListas exListas = new ExListas();
+		List<String> cores = exListas.minhasCores();
+		int contagem = exListas.contaArray(cores);
+		Assert.assertEquals(3, contagem);
+	}
 
+	@Test
+	public void deveInserir3ItensListaERemoverSegundo() {
+		ExListas exListas = new ExListas();
+		List<String> itens = exListas.addListArray("Bola", "Raquete", "Tênis");
+		Assert.assertEquals(2, itens.size());
+		Assert.assertTrue(itens.contains("Bola"));
+		Assert.assertTrue(itens.contains("Tênis"));
+	}
+	
+	@Test
+	public void deveOrdenarMinhasCoresFavoritas() {
+		ExListas exListas = new ExListas();
+		List<String> coresOrdenadas = exListas.ordenaArrayList();
+		Assert.assertEquals("Azul", coresOrdenadas.get(0));
+		Assert.assertEquals("Preto", coresOrdenadas.get(1));
+		Assert.assertEquals("Verde", coresOrdenadas.get(2));
+	}
+	
+	@Test 
+	public void deveReceberUmaListaEDeletarUmItem() {
+		ExListas exListas = new ExListas();
+		List<String> cores = exListas.minhasCores();
+		List<String> coresAtualizadas = exListas.recebeListaDeletaItem(cores, "Azul");
+		Assert.assertEquals(2, coresAtualizadas.size());
+		Assert.assertTrue(coresAtualizadas.contains("Verde"));
+		Assert.assertTrue(coresAtualizadas.contains("Preto"));
+	}
+	
+	@Test
+	public void deveOrdenarInvertidoMinhasCoresFavoritas() {
+		ExListas exListas = new ExListas();
+		List<String> coresOrdemInversa = exListas.inverteArrayList();
+		Assert.assertEquals("Verde", coresOrdemInversa.get(0));
+		Assert.assertEquals("Preto", coresOrdemInversa.get(1));
+		Assert.assertEquals("Azul", coresOrdemInversa.get(2));
+	}
+	
+	@Test
+	public void deveSomarNumerosEmUmaLista() {
+		ExListas exListas = new ExListas();
+		List<Integer> listaNum = exListas.meusNumeros();
+		int soma = exListas.somaLista(listaNum);
+		Assert.assertEquals(15, soma);
+	}
 }
+	
+	
